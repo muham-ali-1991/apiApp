a26c1bb49496e1e905f36aaec89d16b754d7efa7
muham-ali-1991 <muham.ali.dev@gmail.com>
2024-03-30 03:16:43 +0500

    finalCommit

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,31 @@ import './App.css';
 function App() {
 
   const [homeData, sethomeData] = useState([]);
+  const [commentsData, setcommentsData] = useState([]);
+  const [picturesData, setpicturesData] = useState([]);
 
   const loadHome = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     const conv = await res.json();
     sethomeData(conv);
+    setcommentsData([]);
+    setpicturesData([]);
+  }
+
+  const loadComments = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+    const conv = await res.json();
+    setcommentsData(conv);
+    sethomeData([]);
+    setpicturesData([]);
+  }
+
+  const loadPictures = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const conv = await res.json();
+    setpicturesData(conv);
+    sethomeData([]);
+    setcommentsData([]);
   }
 
   return (
@@ -25,10 +45,10 @@ function App() {
                 <a className="nav-link active" aria-current="page" href="#" onClick={loadHome}>Load Posts</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
+                <a className="nav-link" href="#" onClick={loadComments}>Load Comments</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
+                <a className="nav-link" href="#" onClick={loadPictures}>Load Pictures</a>
               </li>
             </ul>
           </div>
@@ -48,7 +68,33 @@ function App() {
         }
       </div>
 
-      
+      <div>
+        {
+          commentsData.map((d) => (
+            <div key={d.id} className="card">
+              < div className="card-body">
+                < h5 className="card-title">{d.name}</h5>
+                <p className="card-text">{d.body}</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div>
+        {
+          picturesData.map((d) => (
+            <div class="card" >
+              <img src={d.url} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                  <h5 class="card-title">d.title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+          ))
+        }
+      </div>
 
     </>
   )
